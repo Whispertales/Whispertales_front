4d615165b9da963b88b2d00042b1edf0ca64502a
Coconut8201 <cocom714kv@gmail.com>
2023-09-01 17:02:57 +0800

    你好，討厭的人類

--- src/view/SD.tsx
+++ src/view/SD.tsx
@@ -3,7 +3,7 @@ import Chatroom from  '../components/Chatroom';
 export default function SD(){
     return(
         <div>
-            <p>你好，討厭的人類</p>
+            <p>你好，可愛的人類</p>
             <Chatroom></Chatroom>
         </div>
     );
