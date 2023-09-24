a19862a2d2f11fbf103b2be551279403e12ddb60
Coconut8201 <cocom714kv@gmail.com>
2023-09-25 04:33:54 +0800

    微修聊天室

--- src/components/Chatroom.tsx
+++ src/components/Chatroom.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState, useRef } from 'react';
 import {io, Socket} from 'socket.io-client';
 import { apis } from '../utils/tools/api';
 
+import '../styles/Chatroom.css';
+
 import Image from './Image';
 
 export default function Chatroom(){
@@ -56,7 +58,7 @@ export default function Chatroom(){
                     </li>
                 ))}
             </ul>
-            <button onClick={handleClear}>Clear Chat</button>
+            <button onClick={handleClear} >Clear Chat</button>
             <Image className='imageleft' inputValue={inputValue} handleSubmit_Chatroom={handleSubmit_Chatroom} handleInputChange_Chatroom={handleInputChange_Chatroom}></Image>
         </div>
     );
