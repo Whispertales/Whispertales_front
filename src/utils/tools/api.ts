232ba7b6e0ca24a3a9625f6cf57c60459d21ca11
張宜蓁 <cocom714kv@gmail.com>
2024-07-18 16:43:37 +0800

    更新前端網頁ui，部分展示圖片

--- src/utils/tools/api.ts
+++ src/utils/tools/api.ts
@@ -1,25 +1,23 @@
 // back_port = 7943;
 
 export const enum apis {
-    //dev
-    GetStoryList = `http://localhost:7943/story/getstorylist_fdb`,
-    LLMGenStory = `http://localhost:7943/story/llm/genstory`,
-    GenImagePrompt = `http://localhost:7943/story/llm/genimageprompt`,
+    // //dev
+    // GetStoryList = `http://localhost:7943/story/getstorylist_fdb`,
+    // LLMGenStory = `http://localhost:7943/story/llm/genstory`,
+    // GenImagePrompt = `http://localhost:7943/story/llm/genimageprompt`,
     
+    // //以下為測試功能
+    // GetNewaudio = `http://163.13.202.120:9880`,
+    // getAllSDModel = `http://163.13.202.120:7860/sdapi/v1/sd-models`,
+
+    //use
+    GetStoryList = `http://163.13.202.120:7943/story/getstorylist_fdb`,
+    LLMGenStory = `http://163.13.202.120:7943/story/llm/genstory`,
+    GenImagePrompt = `http://163.13.202.120:7943/story/llm/genimageprompt`,
+
     //以下為測試功能
-    GetNewaudio = `http://163.13.202.120:9880`
-
-    // //use 192.168.1.26
-    // ImageBase64Url = `http://192.168.1.26:7943/image`,
-    // SocketioServer = `http://192.168.1.26:2764`,
-    // GetImagePrompt = `http://192.168.1.26:7943/prompt`,
-    // GetStory = `http://192.168.1.26:7943/story`,
-
-    // //use2 163.13.201.153
-    // ImageBase64Url = `http://163.13.201.153:7943/image`,
-    // SocketioServer = `http://163.13.201.153:2764`,
-    // GetImagePrompt = `http://163.13.201.153:7943/prompt`,
-    // GetStory = `http://163.13.201.153:7943/story`,
+    GetNewaudio = `http://163.13.202.120:9880`,
+    getAllSDModel = `http://163.13.202.120:7860/sdapi/v1/sd-models`,
 
 }
 
