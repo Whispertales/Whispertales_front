57075baae06365e86af2ce7230c5cfb87d07709a
Coconut8201 <cocom714kv@gmail.com>
2024-07-25 01:59:07 +0800

    update

--- src/utils/sdmodel_list.ts
+++ src/utils/sdmodel_list.ts
@@ -1,28 +1,27 @@
 export interface sdmodel{
     sd_name: string,
-    show_name:string,
-    image_name:string
+    show_name: string,
+    image_path: string;
 }
 
 export const sdmodel_list: sdmodel[] = [
-    { sd_name: `AnythingXL_xl.safetensors [8421598e93]`, show_name: `超可愛動漫風格`, image_name:`AnythingXL_xl_image`},
-    { sd_name: `rachelWalkerStyle_v1.ckpt`, show_name: `水彩畫風格`, image_name:`rachelWalkerStyle_v1_image`},
-    { sd_name: `splatterPunkXL_v10.safetensors`, show_name: `賽博龐克風格`, image_name:`splatterPunkXL_v10_image`},
-    { sd_name: `oxalisAnimeScreencap_animeScreencap.safetensors`, show_name: `日本動漫風格`, image_name:`oxalisAnimeScreencap_animeScreencap_image`},
-    { sd_name: `cartoonmix_v10.safetensors`, show_name: `3D卡通風格`, image_name:`cartoonmix_v10_image`},
-    { sd_name: `disneyPixarCartoon_v10.safetensors`, show_name: `皮克斯、迪士尼卡通風格`, image_name:`disneyPixarCartoon_v10_image`},
-    { sd_name: `90sCartoonXL_v09Alpha.safetensors [9b723603f2]`, show_name: `90年代卡通風格`, image_name:`90sCartoonXL_v09Alpha_image`},
-    { sd_name: `cuteCATCuteCitron_v2.safetensors`, show_name: `Q版漫畫風格`, image_name:`cuteCATCuteCitron_v2_image`},
-    { sd_name: `fantasyWorld_v10.safetensors`, show_name: `奇幻風格`, image_name:`fantasyWorld_v10_image`},
-    { sd_name: `flat2DAnimerge_v45Sharp.safetensors`, show_name: `2D動漫風格`, image_name:`flat2DAnimerge_v45Sharp_image`},
-    { sd_name: `mechaMusumeMerged_mechaMusumeVividSoft.safetensors`, show_name: `機械風格`, image_name:`mechaMusumeMerged_mechaMusumeVividSoft_image`},
-    { sd_name: `slatePencilMix_v10.safetensors`, show_name: `素描風格`, image_name:`slatePencilMix_v10_image`},
-    { sd_name: `ChineseInkComicStrip_v10.ckpt`, show_name: `中國傳統水墨畫`, image_name:`ChineseInkComicStrip_v10_image`},
-    { sd_name: `cemremixRealistic_v10Pruned.safetensors`, show_name: `真實感`, image_name:`cemremixRealistic_v10Pruned_image`},
-    { sd_name: `pixelArtDiffusionXL_spriteShaper.safetensors`, show_name: `像素風`, image_name:`pixelArtDiffusionXL_spriteShaper_image`},
-    { sd_name: `HuixincosmicAllInOne_sd15V10.safetensors`, show_name: `巴洛克`, image_name:`HuixincosmicAllInOne_sd15V10_image`},
-    { sd_name: `handDrawnPortrait_v10.safetensors`, show_name: `手繪`, image_name:`handDrawnPortrait_v10_image`},
-    { sd_name: `splatterPunkNeon_v17Illustration.safetensors`, show_name: `賽博龐克風格`, image_name:`splatterPunkNeon_v17Illustration_image`},
-    { sd_name: `solsticeAKoreanWebtoon_v10AreumNoemaFp16.safetensors`, show_name: `韓漫`, image_name:`solsticeAKoreanWebtoon_v10AreumNoemaFp16_image`},
-    { sd_name: `paintersCheckpointOilPaint_v11.safetensors`, show_name: `油畫`, image_name:`paintersCheckpointOilPaint_v11_image`}
+    { sd_name: 'AnythingXL_xl.safetensors [8421598e93]', show_name: '超可愛動漫風格', image_path: '/src/images/AnythingXL_xl_image.jpeg' },
+    { sd_name: 'rachelWalkerStyle_v1.ckpt', show_name: '水彩畫風格', image_path: '/src/images/rachelWalkerStyle_v1_image.jpeg' },
+    { sd_name: 'splatterPunkXL_v10.safetensors', show_name: '賽博龐克風格', image_path: '/src/images/splatterPunkXL_v10_image.jpeg' },
+    { sd_name: 'oxalisAnimeScreencap_animeScreencap.safetensors', show_name: '日本動漫風格', image_path: '/src/images/oxalisAnimeScreencap_animeScreencap_image.jpeg' },
+    { sd_name: 'cartoonmix_v10.safetensors', show_name: '3D卡通風格', image_path: '/src/images/cartoonmix_v10_image.jpeg' },
+    { sd_name: 'disneyPixarCartoon_v10.safetensors', show_name: '皮克斯、迪士尼卡通風格', image_path: '/src/images/disneyPixarCartoon_v10_image.jpeg' },
+    { sd_name: '90sCartoonXL_v09Alpha.safetensors [9b723603f2]', show_name: '90年代卡通風格', image_path: '/src/images/90sCartoonXL_v09Alpha_image.jpeg' },
+    { sd_name: 'cuteCATCuteCitron_v2.safetensors', show_name: 'Q版漫畫風格', image_path: '/src/images/cuteCATCuteCitron_v2_image.jpeg' },
+    { sd_name: 'fantasyWorld_v10.safetensors', show_name: '奇幻風格', image_path: '/src/images/fantasyWorld_v10_image.jpeg' },
+    { sd_name: 'flat2DAnimerge_v45Sharp.safetensors', show_name: '2D動漫風格', image_path: '/src/images/flat2DAnimerge_v45Sharp_image.jpeg' },
+    { sd_name: 'mechaMusumeMerged_mechaMusumeVividSoft.safetensors', show_name: '機械風格', image_path: '/src/images/mechaMusumeMerged_mechaMusumeVividSoft_image.jpeg' },
+    { sd_name: 'slatePencilMix_v10.safetensors', show_name: '素描風格', image_path: '/src/images/slatePencilMix_v10_image.jpeg' },
+    { sd_name: 'ChineseInkComicStrip_v10.ckpt', show_name: '中國傳統水墨畫', image_path: '/src/images/ChineseInkComicStrip_v10_image.jpeg' },
+    { sd_name: `cemremixRealistic_v10Pruned.safetensors`, show_name: `真實感`, image_path: '/src/images/cemremixRealistic_v10Pruned_image.jpeg' },
+    { sd_name: `pixelArtDiffusionXL_spriteShaper.safetensors`, show_name: `像素風`, image_path: '/src/images/pixelArtDiffusionXL_spriteShaper_image.jpeg' },
+    { sd_name: `handDrawnPortrait_v10.safetensors`, show_name: `手繪`, image_path: '/src/images/handDrawnPortrait_v10_image.jpeg' },
+    { sd_name: `splatterPunkNeon_v17Illustration.safetensors`, show_name: `賽博龐克風格`, image_path: '/src/images/splatterPunkNeon_v17Illustration_image.jpeg' },
+    { sd_name: `solsticeAKoreanWebtoon_v10AreumNoemaFp16.safetensors`, show_name: `韓漫`, image_path: '/src/images/solsticeAKoreanWebtoon_v10AreumNoemaFp16_image.jpeg' },
+    { sd_name: `paintersCheckpointOilPaint_v11.safetensors`, show_name: `油畫`, image_path: '/src/images/paintersCheckpointOilPaint_v11_image.jpeg' }
 ]
